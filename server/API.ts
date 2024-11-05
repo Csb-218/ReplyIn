31c8ce4050cefb1762b28bcab7407bcea835c23b
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2024-11-05 17:27:19 +0530

    feat : Added two functions getCandidate and generateChatResponse

---
+++ server/API.ts
@@ -0,0 +1,43 @@
+
+
+
+export async function getCandidate(supabase:any){
+ 
+   
+   const { data, error } = await supabase
+  .from('candidates')
+  .select()
+  .eq('id', 1)
+
+  alert(error)
+
+  console.error(error)
+  return data
+
+}
+
+export async function generateChatResponse(data:any,JD:string,client:any){
+
+    console.log(JSON.stringify(data))
+    
+      const response = await client.chat.complete({
+        model: 'mistral-large-latest',
+        messages: [{
+          role: 'user', 
+    
+          // content:Hey Mistral, Can you read and print a json ? If yes read and print it . ### JSON: ${JSON.stringify(data)}
+    
+          content:`
+          # Job description : ${JD} 
+          # Candidate : ${JSON.stringify(data)} 
+          
+          # Write a suitable cover letter for the candidate using candidate's information. Don't provide a template. Tailor it according to the provided candidate's information. Use minute details such as candidate's address , phone number, candidate's name etc. to write a good cover letter. 
+          ###Caution : Wherever you find null leave that field.`
+           }],
+      });
+
+      return response.choices?.[0].message?.content
+  
+    
+}
+
