04857e86e8a463b4e9c0ee0622ff7c1ec9fb1e7f
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2024-11-07 02:54:53 +0530

    Update App.tsx

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -2,35 +2,38 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 
-function App(){
+function App() {
 
 
 
   return (
     <>
-      <h1 id='div'>Hi 👋🏻 </h1>
-      <h2> I am 
-        <span
-        className = 'cursor-pointer mx-1 text-yellow-300 font-bold'
-        onClick={()=>window.open('https://github.com/Csb-218/ReplyIn')}
-        >
+      <div className='  '>
+        <p id='div' className='text-3xl'>Hi 👋🏻 </p>
+        <h2> I am
+          <span
+            className='cursor-pointer mx-1 text-yellow-300 font-bold'
+            onClick={() => window.open('https://github.com/Csb-218/ReplyIn')}
+          >
 
-          ReplyIn
-        </span>
-        
-        
-         - An AI text generator</h2>
+            ReplyIn
+          </span>
 
-      <h2>
-         Open 
-         <span
-         className = 'cursor-pointer mx-1 underline'
-         onClick={()=>window.open('https://linkedin.com')}
-         >
-         LinkedIn
-         </span>
-         to start !
-      </h2>
+
+          - An AI text generator</h2>
+
+        <h2>
+          Open
+          <span
+            className='cursor-pointer mx-1 underline'
+            onClick={() => window.open('https://linkedin.com')}
+          >
+            LinkedIn
+          </span>
+          to start !
+        </h2>
+      </div>
+
     </>
   );
 }
