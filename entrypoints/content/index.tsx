15bce50fc5a770cb176aaf5cf3695e4da795e4d8
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2024-11-09 01:08:30 +0530

    fix : remove unwanted LOC

--- entrypoints/content/index.tsx
+++ entrypoints/content/index.tsx
@@ -2,26 +2,10 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import generateIcon from '~/assets/Frame.svg';
 import type { ContentScriptContext } from "wxt/client";
-import "~/assets/tailwind.css";
-import { Mistral } from "@mistralai/mistralai";
-import { createClient } from '@supabase/supabase-js';
-import { Database } from "@/utils/supabase";
-import { getCandidate,generateChatResponse } from '@/server/API';
+import './styles.css'
 
 
-// Load environment variables from .env file
-
-
-const client = new Mistral({
-  apiKey: 'PyBqviCN1xwhAv2V6677tjxsl19Pk9ka'
-});
-
-const db_url: string = 'https://bxmbmbjyekriyoefgxwq.supabase.co'
-const db_api_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ4bWJtYmp5ZWtyaXlvZWZneHdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg3NTE4ODMsImV4cCI6MjA0NDMyNzg4M30.jZmls3wISihsyNtXc6TNK7JCaFimqa0i15f-_ELJsqs'
-// Create a single supabase client for interacting with your database
-const supabase = createClient<Database>(db_url, db_api_key)
-
-
+let AboutTheJobSection : string;
 
 export default defineContentScript({
   matches: ["*://*.linkedin.com/*", '*://*.wellfound.com/*'],
@@ -48,110 +32,8 @@ export default defineContentScript({
         img.style.cursor = 'pointer'
 
         img.onclick = () => {
-          alert('listening')
-          ui.mount();
-
-
-          
-          // messageBox.textContent = '' 
-        }
-
-        // document.body.style.backgroundColor = 'black'
-        alert(document.body.style.backgroundColor)
-        document.body.style.backgroundImage = generateIcon
-        // document.body.appendChild(img)
-
-        // async function getCandidate() {
-
-        //   const { data, error } = await supabase
-        //     .from('candidates')
-        //     .select()
-        //     .eq('id', 1)
-
-        //   return data
-
-        // }
-
-        // async function generateChatResponse(data: any, JD: string) {
-
-        //   console.log(JSON.stringify(data))
-
-        //   const response = await client.chat.complete({
-        //     model: 'mistral-large-latest',
-        //     messages: [{
-        //       role: 'user',
-        //       content: `
-        //          # Job description : ${JD} 
-        //          # Candidate : ${JSON.stringify(data)} 
-                 
-        //          # Write a suitable cover letter for the candidate using candidate's information. Don't provide a template. Tailor it according to the provided candidate's information. Use minute details such as candidate's address , phone number, candidate's name etc. to write a good cover letter. 
-        //          ###Caution : Wherever you find null leave that field.`
-        //     }],
-        //   });
-
-        //   alert(response)
-        //   if (response.choices && response.choices.length > 0 && response.choices[0].message) {
-        //     return response.choices[0].message.content;
-        //   } else {
-        //     console.error("API response is missing 'choices' or 'message' content.");
-        //     return ""; // or handle the error as needed
-        //   }
-
-
-        // }
-        
-
-
-        // insert generate icon on focus
-        async function listenFocus(event: Event) {
-          // alert('listening')
-          const messageBox = event.currentTarget as HTMLElement;
-          img.onclick = () => {
-            alert('listening')
-            // ui.mount();
-
-
-            
-            // messageBox.textContent = '' 
-          }
-          const blank:HTMLDivElement = document.createElement('div');
-          blank.innerHTML = `
-          Hi
-          `
-          blank.style.position = 'absolute'
-          blank.style.top = '10px'
-          blank.style.left = '10px'
-          blank.style.height = '767px'
-          blank.style.backgroundColor = 'blue'
-
-          // document.body.innerHTML = '<></>'
-          // document.body.appendChild(img)
-
-          // Select the <h2> element that contains the text "About the job"
-          const heading = Array.from(document.querySelectorAll('h2')).find(
-            el => el.textContent?.trim() === "About the job"
-          );
-
-          if (heading) {
-            console.log(heading);
-            // messageBox.innerText = "csb"
-            const AboutTheJobSection = heading.parentNode?.textContent ? heading.parentNode?.textContent : "";
-
-            // alert("start")
-            const candidate: any = await getCandidate(supabase)
-            // alert(candidate)
-            ui.mount();
-            const chat: any = await generateChatResponse(candidate, AboutTheJobSection,client)
-            alert(chat+' hi')
-            messageBox.innerText = chat ? chat : AboutTheJobSection
-
-          }
-        }
-
-        // remove generate icon on blur
-        function removeFocus(event: Event) {
-          const messageBox = event.currentTarget as HTMLElement;
-          // img && messageBox.removeChild(img)
+          ui.mount()
+          alert('mounted')
         }
 
         // mutation observer
@@ -165,14 +47,20 @@ export default defineContentScript({
           if (messageBox) {
 
             messageBox.parentElement?.appendChild(img)
-           
-            alert(messageBox.toString())
-            // listen to focus
-            // messageBox?.addEventListener("focus", listenFocus)
+
+            // Select the <h2> element that contains the text "About the job"
+          const heading = Array.from(document.querySelectorAll('h2')).find(
+            el => el.textContent?.trim() === "About the job"
+          );
+
+            AboutTheJobSection = heading?.parentNode?.textContent ? heading?.parentNode?.textContent : "";
+
+            // chrome.runtime.sendMessage({ AboutTheJobSection });
             // Stop observing once the element is found
             observer.disconnect();
-            // listen to blur
-            // messageBox?.addEventListener("blur", removeFocus)
+
+            console.log('disconnected')
+            
           }
         });
 
@@ -200,20 +88,25 @@ export default defineContentScript({
 
 function createUi(ctx: ContentScriptContext) {
   return createShadowRootUi(ctx, {
-    name: "Generative Text Editor Modal",
+    name: "generative-text-editor-modal",
     position: "inline",
     anchor: "body",
     append: "first",
     onMount: (container) => {
-
+      
+      alert('mount')
       // Don't mount react app directly on <body>
       const wrapper = document.createElement("div");
       container.append(wrapper);
 
       const root = ReactDOM.createRoot(wrapper);
-        root.render(<App />);
+        root.render(<App JD={AboutTheJobSection}/>);
         return { root, wrapper };
 
+    },
+    onRemove: (elements) => {
+      elements?.root.unmount();
+      elements?.wrapper.remove();
     },
   });
 }
