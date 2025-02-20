f306cc86a3c18907b8ea20132916cb5514f3c631
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-02-20 17:25:33 +0530

    Update background.ts

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,5 +1,6 @@
+import {googleLogin,listenUpdated} from '../utils/backgroundFunctions'
 
-export default defineBackground(async() => {
+export default defineBackground(async () => {
   console.log('Hello csb !', { id: browser.runtime.id });
 
   chrome.runtime.onInstalled.addListener(() => {
@@ -8,54 +9,17 @@ export default defineBackground(async() => {
 
   var isContentScriptReady: boolean = true;
 
-  async function listenCreated(tab: any) {
-    console.log('yes')
-     
-    
-      if ( tab.url?.includes("wellfound.com")) {
-
-        console.log("tab detected (onCreated):", tab.url, tab.id);
-
-        // Send a message to the content script in the new tab
-        chrome.tabs.sendMessage(tab.id!, { message: "PageUpdated" }, async (response) => {
-
-          console.log('Message sent to content script', response);
-
-        });
-      }
-    
-
-  }
-
-  function listenUpdated(tabId: number, changeInfo: any, tab: any) {
-
-    if (isContentScriptReady) {
-
-      if (changeInfo.status === 'complete' && tab.url?.includes("wellfound.com/jobs?job_listing_id")) {
-
-        console.log(" tab detected(onUpdated):", tab.url,tabId);
-        // Send a message to the content script in the current tab
-        chrome.tabs.sendMessage(tabId, { message: "PageUpdated" }, async (response) => {
-
-          console.log('Message sent to content script', response);
-
-        })
-      }
-    }
-  }
-
-
-
-  // Listen to new tab creation 
-  // chrome.tabs.onCreated.addListener(listenCreated);
   // Listen to new tab updation 
   chrome.tabs.onUpdated.addListener(listenUpdated);
 
   // Handle tab closure
   chrome.tabs.onRemoved.addListener((tabId) => {
-    
     console.log(`Tab ${tabId} was closed.`);
   });
 
+  const CLIENT_ID = import.meta.env.WXT_GOOGLE_CLIENT_ID;
+  const SCOPES = ['email', 'profile'];
+
+  chrome.runtime.onMessage.addListener(googleLogin);
 });
 
