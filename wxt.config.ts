bec3ec6192377099ae054f5649faffa48253b02e
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-02-20 17:10:16 +0530

    feat : permissions , key , oauth2 and content_security_policy added

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,8 +4,19 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
   manifest: {
-    permissions: ['storage',"activeTab","scripting","tabs","webNavigation"],
-    host_permissions: ['*://*.wellfound.com/*','*://*.internshala.com/*'],
-    
+    permissions: ['storage', 'activeTab', 'scripting', 'tabs', 'webNavigation', 'identity'],
+    host_permissions: ['*://*.wellfound.com/*', '*://*.internshala.com/*'],
+    version: "3.1.2.4567",
+    key:"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuFXNdKN5BoytbK0GLTGL4o6H/MEx6F9P3L8FfTJRdmfH7FbmzGOOcrcorfSmYBKWuW74dSXQL4aqw+ChzR/so7lZChwoDRAFdvErU64l+Z+xwWVlbra8JjAeaZjjQRUplwh30gf+Y+s2t1i9tCtAaa7qqN5JW8F8u4eVihHDn54kAFMLj4H4Xl3FlNWzQry+WZdX/6GPuWyS5KRolM1ZHNCF2IvrXkDJ1DR8p87NfwI6r98m8fdrnUhIOT5AZH7uFI25QhCjvdZ2sRbIRiu24fQ8yP/1rJICXFTCVT5nOU/qmxRE9WNjUcxpF9nZAwD+XJ1C3JYvNGnEqlZD/YiMUwIDAQAB",
+    oauth2: {
+      client_id: "113439342527-kliuhrfour83snj76k0fsuch50gu5rta.apps.googleusercontent.com",
+      scopes: [
+        'https://www.googleapis.com/auth/userinfo.email',
+        'https://www.googleapis.com/auth/userinfo.profile'
+      ]
+    },
+    content_security_policy: {
+      extension_pages: "script-src 'self' 'wasm-unsafe-eval' http://localhost:3000 http://localhost:3001; object-src 'self'"
+    }
   }
 });
