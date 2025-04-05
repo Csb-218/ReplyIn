e47ad9a8ff7e6a49438ab4a532b9341c5ab1ed0d
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-04-05 13:24:48 +0530

    Create index.ts

---
+++ src/assets/index.ts
@@ -0,0 +1,8 @@
+import wellfound from './platforms/wellfound.png';
+import linkedin from './platforms/linkedin.svg';
+import gmail from './platforms/gmail.png';
+import internshala from './platforms/internshala.jpeg';
+import brand_logo_2 from "./brand_logo_2.svg";
+
+
+export { wellfound, linkedin, gmail, internshala , brand_logo_2 };
