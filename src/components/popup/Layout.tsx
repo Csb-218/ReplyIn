c135a05705de5df84a398aa84250469f9731eb45
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-04-05 13:24:17 +0530

    Update Layout.tsx

--- src/components/popup/Layout.tsx
+++ src/components/popup/Layout.tsx
@@ -1,19 +1,38 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+import { useState } from 'react';
+import {brand_logo_2} from "@/assets"
 import {Outlet} from "react-router-dom"
 
 const Layout = () => {
+  const [isHovered, setIsHovered] = useState(false);
   return (
     <>
-    <p id='div' className='text-3xl'>Hi 👋🏻 </p>
-    <h2> I am
-      <span
-        className='cursor-pointer mx-1 text-yellow-300 font-bold'
-        onClick={() => window.open('https://github.com/Csb-218/ReplyIn')}
-      >
-        ReplyIn
-      </span>
-      - An AI text generator
-    </h2>
+    <div className="bg-transparent">
+        <motion.div
+          animate={{ rotate: isHovered ? 360 : 0 }}
+          transition={{ duration: 0.5 }}
+          onHoverStart={() => setIsHovered(true)}
+          onHoverEnd={() => setIsHovered(false)}
+          className="w-20 h-20 mx-auto mb-6"
+        >
+          <img
+            src={brand_logo_2}
+            alt="ReplyIn Logo"
+            className="w-full h-full "
+          />
+        </motion.div>
+
+
+        <h1 className="text-4xl font-bold bg-gradient-to-r from-emerald-600 to-teal-600 bg-clip-text text-transparent mb-4">
+          Covlet
+        </h1>
+        
+        <p className="text-gray-600 mb-8">
+          Your AI-powered cover letter assistant that helps you stand out
+        </p>
+ 
+    
+    </div>
     <Outlet/>
   </>
   )
